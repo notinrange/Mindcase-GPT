26184f346ddf4dec7b8d31cc86382289688eeb09
Rahul Kumar <rahulkumar_ec20b13_38@dtu.ac.in>
2024-06-05 13:19:17 +0530

    second commit

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -29,67 +29,67 @@ export default function MessagesPage(){
     );
 
 
-function RenderForm() {
-  
-  return (
-    <form onSubmit={(event)=>{
-      event.preventDefault();
-      handleSubmit(event,{
-        data:{
-          promt:input
-        }
-      })
-    }}
-    className="w-full h-20px flex flex-row gap-2 items-center h-full"
-    >
-      <Input type="text" 
-      placeholder={isLoading ? "Generating. . ." : "enter your prompt . . ." }
-      value={input}
-      disabled = {isLoading}
-      onChange={handleInputChange}
-      className="rounded-full "
-      />
-      <Button type = "submit"
-      className="rounded-full shadow-md border flex flex-row"
-      > 
-      {isLoading ? (<Loader2 className="p-3 h-10 w-10 stroke-stone-500 animate-spin"/> )
-      :(
-        <Send className = "p-3 h-10 w-10 stroke-stone-500"/>
-      )}
-      </Button>
+      function RenderForm() {
+        
+        return (
+          <form onSubmit={(event)=>{
+            event.preventDefault();
+            handleSubmit(event,{
+              data:{
+                promt:input
+              }
+            })
+          }}
+          className="w-full h-20px flex flex-row gap-2 items-center h-full"
+          >
+            <Input type="text" 
+            placeholder={isLoading ? "Generating. . ." : "enter your prompt . . ." }
+            value={input}
+            disabled = {isLoading}
+            onChange={handleInputChange}
+            className="rounded-full "
+            />
+            <Button type = "submit"
+            className="rounded-full shadow-md border flex flex-row"
+            > 
+            {isLoading ? (<Loader2 className="p-3 h-10 w-10 stroke-stone-500 animate-spin"/> )
+            :(
+              <Send className = "p-3 h-10 w-10 stroke-stone-500"/>
+            )}
+            </Button>
 
-    </form>
-    );
-}
+          </form>
+          );
+      }
 
-  function RenderMessage() {
-    return (
-      <div
-    className="flex flex-col-reverse w-full text-left mt-4 gap-4 whitespace-pre-wrap">
-    {messages.map((m,index)=>{
-      return <div 
-      className={`p-4 shadow-md border flex flex-row rounded-md ml-10 relative ${
-        m.role === 'user' ? "bg-slate-500" : "" 
-      }`}>
-        {m.content}
-        {m.role === 'user' ?( 
-        <User2 className = "absolute top-2 -left-10 border rounded-full p-1 shadow-lg "/>) 
-        : 
-        (
-        <Bot className={`absolute top-2 -left-10 border rounded-full p-1 shadow-lg 
-        ${
-          isLoading && index === messages.length - 1 ? "animate-bounce" : ""
-        }`}
-        />
-      )}
+      function RenderMessage() {
+        return (
+          <div
+        className="flex flex-col-reverse w-full text-left mt-4 gap-4 whitespace-pre-wrap">
+        {messages.map((m,index)=>{
+          return <div 
+          className={`p-4 shadow-md border flex flex-row rounded-md ml-10 relative ${
+            m.role === 'user' ? "bg-slate-500" : "" 
+          }`}>
+            {m.content}
+            {m.role === 'user' ?( 
+            <User2 className = "absolute top-2 -left-10 border rounded-full p-1 shadow-lg "/>) 
+            : 
+            (
+            <Bot className={`absolute top-2 -left-10 border rounded-full p-1 shadow-lg 
+            ${
+              isLoading && index === messages.length - 1 ? "animate-bounce" : ""
+            }`}
+            />
+          )}
 
-      </div>
-    })}
-    
+          </div>
+        })}
+        
 
-    </div>
-    );
-    
-  }
+        </div>
+        );
+        
+      }
 
 }
